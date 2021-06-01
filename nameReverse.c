1142a7a2a916c622585f8f5fe015bb6581269d5f
pfox27 <pfox27@gmail.com>
2021-06-01 15:25:43 -0400

    Update

--- nameReverse.c
+++ nameReverse.c
@@ -5,18 +5,16 @@
 
 
 int main(){
-	
+
 	char name[20];
 	printf("Please enter your full name\n");
 	scanf("%[^\n]", name);
 	//printf("Hello %s \n", name);
 	size_t length = strlen(name);
-	for(int count=length; count=0; count--){
+	for(int count=length; count=0; count--){  /* A loop to reverse all the characters in the name string */
 		char character = name[count];
 		printf("Your name reversed is %d", character);
 		}
-		
+
 		return 0;
 }
-
-
