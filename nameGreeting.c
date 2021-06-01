02f178fa3073fdcfbd3e4c780d51be5543ea91bd
pfox27 <pfox27@gmail.com>
2021-06-01 11:43:57 -0400

    Update

--- nameGreeting.c
+++ nameGreeting.c
@@ -4,7 +4,7 @@
 
 int main(){
 	char name[30];
-	printf("Hello, please enter your full name:");
+	printf("Hello, please enter your full name: ");
 //	scanf("%s", name);
 //	printf("Greetings %s\n", name);
 	gets (name);
